d1be05172fb01e96bf8784094335d20c063a0ff4
denismazanov <82777275+denismazanov@users.noreply.github.com>
2022-06-21 19:29:51 +0300

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -3,7 +3,16 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+    /* 
+    Istifadəçinin daxil etdiyi dəyişəni qəbul edirik. 
+    Giriş veriləni String type olur. Riyazi əməliyyatları tətbiq etmək üçün parseInt() metodundan istifadə edərək Number type-a çevrilməlidir.
+    */
+    let a = parseInt(result.input); // 9
+    let n = 0; 
+
+    for(let i = 1;i<a;i++){
+        if (a%i===0 && i%2!==0){
+            console.log(i)
+        }
+    };
 });
